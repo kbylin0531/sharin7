39e9022eff745fd44e5e8dc882509b7cc7f1fc08
linzhv <784855684@qq.com>
2016-10-20 19:16:05 +0800

    晓

---
+++ Config/storage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus
+ * Date: 16-10-10
+ * Time: 下午4:16
+ */
+return [
+    'WRITABLE_SCOPE' =>
+        array (
+            0 => SR_PATH_BASE.'Runtime/',
+            1 => SR_PATH_BASE.'Public/',
+        ),
+];
